25039089947156773393dd383c05154c8b17c21f
Naina Rajan <rajan.nai@northeastern.edu>
2024-01-10 11:55:10 -0500

    React coursework 2

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-/***
- *
- * <div id="parent">
- *          <div id="child">
- *              <h1>I'm h1 tag</h1>
- *              <h2>I'm h2 tag</h2>
- *          </div>
- *          <div id="child1">
- *              <h1>I'm h1 tag</h1>
- *              <h2>I'm h2 tag</h2>
- *          </div>
- * </div>
- *
- *
- * ReactElement(Object) => HTML(Browser Understands)
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading1 = React.createElement(
   "h1",
@@ -26,12 +11,12 @@ const roott = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h1", {}, "This is tag 1"),
+    React.createElement("h2", {}, "This is tag 2"),
   ]),
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h1", {}, "This is tag 3"),
+    React.createElement("h2", {}, "This is tag 4"),
   ]),
 ]);
 roott.render(parent);
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # React Coursework
 
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement - (Refreshing on saving)
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting - split file
+- Differential Bundling - Support older Browser
+- Diagnostic - view of error
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
